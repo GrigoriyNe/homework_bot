9979401e01766aedaedbab437b3c2e4475ca572a
Grigoriy N <b.grishkevitch@yandex.ru>
2024-06-29 14:41:40 +0500

    for write time

--- homework.py
+++ homework.py
@@ -1,11 +1,20 @@
-...
+import logging
+import time
+import os
+from os import environ
+import requests
+from pprint import pprint
+
+from dotenv import load_dotenv
+from telebot import TeleBot, types
+
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -19,48 +28,69 @@ HOMEWORK_VERDICTS = {
 }
 
 
-def check_tokens():
-    ...
-
-
 def send_message(bot, message):
-    ...
+    bot.send_message(
+        TELEGRAM_CHAT_ID,
+        message
+    )
 
 
 def get_api_answer(timestamp):
-    ...
+   # params = {'from_date': timestamp}
+    try:
+        homework_statuses = requests.get(ENDPOINT,
+                                         headers=HEADERS,
+                                         params=timestamp
+                                         )
+    except:
+        raise Exception(f'Ошибка при запросе к основному API')
+    try:
+        return homework_statuses.json()
+    except:
+        raise Exception(f'Ошибка')
 
 
 def check_response(response):
-    ...
+    list_works = response['homeworks']
+    homework = list_works[0]
+    return homework
 
 
 def parse_status(homework):
-    ...
-
+    homework_name = homework['homework_name']
+    homework_status = homework['status']
+    verdict = HOMEWORK_VERDICTS[homework_status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
+
+
+def check_tokens():
+    if all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]):
+        return True
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
-
-    # Создаем объект класса бота
-    bot = ...
-    timestamp = int(time.time())
-
-    ...
+    bot = TeleBot(token=TELEGRAM_TOKEN)
+    timestamp = {'from_date': 1549962000}
+    status =''
+    
+    if not check_tokens():
+        raise Exception('Отсутствуют одна или несколько переменных окружения')
 
     while True:
         try:
-
-            ...
+            response = get_api_answer(timestamp)
+            timestamp = response.get('current_date')
+            message = parse_status(check_response(response))
+            print(message)
+            if message != status:
+                send_message(bot, message)
+                status = message
+            time.sleep(RETRY_PERIOD)
 
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+            send_message(bot, message)
 
 
 if __name__ == '__main__':
