c68df95ba424e99c65a8b8197029621fa825b58e
Grigoriy N <b.grishkevitch@yandex.ru>
2024-07-01 01:13:41 +0500

    Second try, work on loggers, name probloms, logic problems.

--- homework.py
+++ homework.py
@@ -81,17 +81,16 @@ def get_api_answer(timestamp):
 
 def check_response(response):
     """."""
-    if homeworks := response.get('homeworks'):
-        try:
-            homework_list = response['homeworks']
-        except KeyError:
-            raise KeyError('Ответ не содержит заданий')
-    if type(homeworks) != list:
-        raise TypeError('Тип списка домашки - не list')
+
+    if homework_list := response.get['homeworks']:
+        if type(homework_list) != list:
+            raise TypeError('Тип списка домашки - не list')
     try:
         homework = homework_list[0]
     except IndexError:
         raise IndexError('В списке домашинх работ нет домашек')
+    except KeyError:
+        raise KeyError('Ответ не содержит заданий')
     return homework
 
 
